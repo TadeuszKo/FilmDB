02f46044d827cdb2820da973bb4f794e995e7978
TadeuszKo <tadeusz792@gmail.com>
2023-01-05 00:27:57 +0100

    zadania git 5 - 10

--- FilmDB/Controllers/FilmController.cs
+++ FilmDB/Controllers/FilmController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmDB.Logic;
+using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace FilmDB.Controllers
 {
     public class FilmController : Controller
     {
+        //FilmManager filmManager = new FilmManager();
         public IActionResult Index()
         {
-            return View();
+            //var random = new Random();
+            //var film = new FilmModel();
+            //{
+            //    Title=$"Rambo{random.Next(1,100)}",
+            //    Year = 1999
+            //};
+            //filmManager.AddFilm(film);
+
+            //dodawanie nowego filmu dobazy danych
+            var manager = new FilmManager();
+            manager.AddFilm(new FilmModel()
+            {
+                ID = 0,
+                Title = "Matrix",
+                Year = 1999
+            });
+
+            //usuwanie filmu z bazy danych
+            //var managerRemove=new FilmManager();
+            //managerRemove.RemoveFilm(2);
+
+            //aktualizacja filmu/rekordu w bazie danych
+            //var managerUpdate = new FilmManager();
+            //managerUpdate.UpdateFilm(new FilmModel()
+            //{
+            //    ID=2,
+            //    Title="Madagaskar",
+            //    Year=2010
+            //});
+
+            //zmiana tytułu filmu
+            var managerZmianTitle = new FilmManager();
+            managerZmianTitle.ChangeTitle(3,"Test");
+            managerZmianTitle.ChangeTitle(4, "Naruto");
+
+                return View();
+                
+
         }
     }
 }
---
+++ FilmDB/Logic/FilmManager.cs
@@ -0,0 +1,91 @@
+﻿using FilmDB.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmDB.Logic
+{
+    public class FilmManager
+    {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                //context.Add(filmModel);
+                //context.SaveChanges();
+                try
+                {
+                    context.Add(filmModel);
+                    context.SaveChanges();
+                }
+                catch (System.Exception)
+                {
+                    filmModel.ID = 0;
+                    context.Add(filmModel);
+                    context.SaveChanges();
+                }
+            }
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            using (var context = new FilmContext()) 
+            {
+                var filmDelete = context.Films.SingleOrDefault(x => x.ID == id);  
+                context.Films.Remove(filmDelete); 
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext()) 
+            {
+                //var filmUpdate= context.Films.SingleOrDefault(x => x.ID == filmModel.ID);
+                context.Films.Update(filmModel);
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            using (var context = new FilmContext()) 
+            {
+                var zmianaTitle = context.Films.Single(x => x.ID == id);
+                if (string.IsNullOrEmpty(newTitle))
+                    zmianaTitle.Title = "Brak Tytułu";
+                else
+                    zmianaTitle.Title = newTitle;
+
+                UpdateFilm(zmianaTitle);
+
+
+
+                
+                //try
+                //{
+                //    zmianaTitle.Title = newTitle;
+                //}
+                //catch (System.Exception)
+                //{
+                //    zmianaTitle.Title = "Brak Tytułu";
+                //}
+                //UpdateFilm(zmianaTitle);
+                
+            }
+            return this;
+        }
+
+        public FilmManager GetFilm(int id)
+        {
+            return null;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            return null;
+        }
+    }
+}
