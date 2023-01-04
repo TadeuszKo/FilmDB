2f19ec9d9a7868a63e3226f7c5b75f28de5f5b21
TadeuszKo <tadeusz792@gmail.com>
2023-01-04 22:29:02 +0100

    zadanie 3 + 4 z git

---
+++ FilmDB/Context/FilmContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace FilmDB.Context
+{
+    //public class filmcontext : dbcontext
+    //{
+    //    protected override void onconfiguring(dbcommandbuilder optionsbuilder)
+    //    {
+    //        var cs = 3;
+    //        optionsbuilder.usesqlserver(cs)
+    //    }
+    //}
+}
