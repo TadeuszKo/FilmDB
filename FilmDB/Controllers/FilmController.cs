ee6c1f2c15b123dd2e710be50ca7341714e927ac
TadeuszKo <tadeusz792@gmail.com>
2023-01-06 15:11:08 +0100

    zadania 14-15

--- FilmDB/Controllers/FilmController.cs
+++ FilmDB/Controllers/FilmController.cs
@@ -9,48 +9,52 @@ namespace FilmDB.Controllers
 {
     public class FilmController : Controller
     {
-        FilmManager manager = new FilmManager();
+        FilmManager filmManager = new FilmManager();
         public IActionResult Index()
         {
-            //var random = new Random();
-            //var film = new FilmModel();
-            //{
-            //    Title=$"Rambo{random.Next(1,100)}",
-            //    Year = 1999
-            //};
-            //filmManager.AddFilm(film);
-
-            //dodawanie nowego filmu dobazy danych
-            //var manager = new FilmManager();
-            manager.AddFilm(new FilmModel()
             {
-                ID = 0,
-                Title = "Matrix",
-                Year = 1999
-            });
+                //var random = new Random();
+                //var film = new FilmModel();
 
-            //usuwanie filmu z bazy danych
-            //var managerRemove=new FilmManager();
-            //managerRemove.RemoveFilm(2);
+                //{
+                //    Title = $"Rambo{random.Next(1, 100)}",
+                //    Year = 1999
+                //};
+                //filmManager.AddFilm(film);
 
-            //aktualizacja filmu/rekordu w bazie danych
-            //var managerUpdate = new FilmManager();
-            //managerUpdate.UpdateFilm(new FilmModel()
-            //{
-            //    ID=2,
-            //    Title="Madagaskar",
-            //    Year=2010
-            //});
+                ////dodawanie nowego filmu dobazy danych
+                ////var manager = new FilmManager();
+                //filmManager.AddFilm(new FilmModel()
+                //{
+                //    ID = 0,
+                //    Title = "Matrix",
+                //    Year = 1999
+                //});
 
-            //zmiana tytułu filmu
-            var managerZmianTitle = new FilmManager();
-            managerZmianTitle.ChangeTitle(3,"Test");
-            managerZmianTitle.ChangeTitle(4, "Naruto");
+                ////usuwanie filmu z bazy danych
+                ////var managerRemove = new FilmManager();
+                //filmManager.RemoveFilm(2);
 
-                return View();
-                
+                ////aktualizacja filmu/ rekordu w bazie danych
+                ////var managerUpdate = new FilmManager();
+                //filmManager.UpdateFilm(new FilmModel()
+                //{
+                //    ID = 2,
+                //    Title = "Madagaskar",
+                //    Year = 2010
+                //});
 
+                ////zmiana tytułu filmu
+                ////var managerZmianTitle = new FilmManager();
+                //filmManager.ChangeTitle(3, "Test");
+                ////managerZmianTitle.ChangeTitle(4, "Naruto");
+            }//wykomentowane
+
+
+            var filmy = filmManager.GetFilms();
+            return View(filmy);
         }
+
         [HttpGet]
         public IActionResult Add() 
         {
@@ -60,9 +64,22 @@ namespace FilmDB.Controllers
         [HttpPost]
         public IActionResult Add(FilmModel filmModel) 
         {
-            manager.AddFilm(filmModel);
+            filmManager.AddFilm(filmModel);
             return RedirectToAction("Index");
+        }
 
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            var film = filmManager.GetFilm(id);
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveConfirm(int id)
+        {
+            filmManager.RemoveFilm(id);
+            return RedirectToAction("Index");
         }
     }
 }
