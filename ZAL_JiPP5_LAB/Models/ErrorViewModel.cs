763117c4cc1deb218bc7086cab4301b0f9e5f77f
TadeuszKo <tadeusz792@gmail.com>
2023-01-22 11:56:37 +0100

    dodanie projektu zaliczenie

---
+++ ZAL_JiPP5_LAB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZAL_JiPP5_LAB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
